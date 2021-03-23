01c21f2e2a6f02ffc38ab12ab2216970dfc600e7
tiavr <tiav.randria@gmail.com>
2021-03-23 14:17:41 +0100

    mss

--- COURS DISTANCIEL/AL/TD1/src/fc/Variables.java
+++ COURS DISTANCIEL/AL/TD1/src/fc/Variables.java
@@ -6,10 +6,12 @@ public class Variables {
     private String[] pays;
 
     public Variables(){
+
         pays = new String[]{"FRANCE", "ALLEMAGNE", "ESPAGNE", "ITALIE", "ANGLETERRE"};
     }
 
     public String[] getPays(){
+
         return pays;
     }
 
--- COURS DISTANCIEL/AL/TD1/src/ui/Window.java
+++ COURS DISTANCIEL/AL/TD1/src/ui/Window.java
@@ -3,11 +3,8 @@ package ui;
 import fc.Variables;
 
 import java.awt.*;
-<<<<<<< HEAD
-=======
 import java.awt.event.KeyEvent;
 import java.util.*;
->>>>>>> 6cb1e4b4023bf2175e15f3389c243b09044b83d9
 import javax.swing.*;
 
 public class Window extends JFrame{
@@ -15,39 +12,25 @@ public class Window extends JFrame{
     private JMenuBar mb = new JMenuBar();
     private JMenuBar mb2 = new JMenuBar();
     private JMenu menu = new JMenu("Pays");
-<<<<<<< HEAD
     private JMenu menu2 = new JMenu("Championnat");
     Interaction interaction;
     public Window(){
+
         run();
     }
 
     public void run(){
-=======
-    private JButton button = new JButton("Valider");
-
-    public Window(Variables variable){
->>>>>>> 6cb1e4b4023bf2175e15f3389c243b09044b83d9
-
-
         setTitle("Actu' Foot");
         setSize(400,500);
         setLocationRelativeTo(null);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-<<<<<<< HEAD
         JPanel content = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JPanel content2 = new JPanel(new FlowLayout());
-=======
         JPanel panel = new JPanel(new FlowLayout());
-
->>>>>>> 6cb1e4b4023bf2175e15f3389c243b09044b83d9
-
 
         JLabel label = new JLabel("Bienvenue sur notre application Actu' Foot de résultats sportifs !");
 
-<<<<<<< HEAD
         content.add(label);
 
         JMenuItem item1 = new JMenuItem("France");
@@ -61,26 +44,12 @@ public class Window extends JFrame{
         for(int i = 0; i < listItem.length; i++){
             menu.add(listItem[i]);
         }
-
+        menu.setMnemonic(KeyEvent.VK_F);
         mb.add(menu);
         mb.setLocation(0,0);
         content.add(mb);
-=======
-        menu.setMnemonic(KeyEvent.VK_F);
-        panel.add(label);
-        for(int i = 0; i < variable.getPays().length; i++){
-            JMenuItem item = new JMenuItem(variable.getPays()[i]);
-            item.addActionListener((event) -> System.exit(0));
-            menu.add(item);
-        }
-        mb.add(menu);
-        mb.setLocation(0,0);
-        panel.add(mb);
 
-        panel.setPreferredSize(new Dimension(100,200));
-        getContentPane().add(panel, BorderLayout.CENTER);
-        this.add(button, BorderLayout.SOUTH);
->>>>>>> 6cb1e4b4023bf2175e15f3389c243b09044b83d9
+
 
         JMenuItem item6 = new JMenuItem("Championnat 1");
         JMenuItem item7 = new JMenuItem("Championnat 2");
